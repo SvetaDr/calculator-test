b98e955b59806ceb94de650456b3f77bb226cf1f
Sviatlana_Drazdova <sviatlana_srazdova@epam.com>
2021-12-07 21:41:16 +0300

    add homework13

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -2,4 +2,8 @@ import com.epam.tat.module4.Calculator;
 
 public class CalculatorTest {
     Calculator calculator = new Calculator();
+
+    public static void main(String[] args) {
+        System.out.println("homeworkbranch");
+    }
 }
